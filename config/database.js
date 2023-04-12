66b2888095cfc64c490668c4ad9cf9d0d1383317
Le Minh Hung <leminhhungtabletennis@gmail.com>
2023-04-12 23:21:44 +0700

    Fix bugs undefinded messageText

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
+    mongoose.set("strictQuery", false);
     const con = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
